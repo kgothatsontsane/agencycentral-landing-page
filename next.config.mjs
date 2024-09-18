6e6f5a542da77c91504b42509887a0062c6c7d15
tristangms <nvisionfactory@gmail.com>
2024-09-18 18:41:43 +0200

    test asset-prefix /

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,9 @@
 const isProduction = process.env.NODE_ENV === 'production';
 const nextConfig = {
     basePath: isProduction ? '/agencycentral-landing-page' : '',
-    assetPrefix: isProduction ? '/agencycentral-landing-page' : '',
+    assetPrefix: isProduction ? '/agencycentral-landing-page/' : '',
     output: 'export',
     distDir: 'deployment',
-    images: {
-        unoptimized: true
-    }, 
     // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
     // trailingSlash: true,
  
