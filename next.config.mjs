b2bea19e688340431fa5d384a61f025b18d61cb7
tristangms <nvisionfactory@gmail.com>
2024-09-18 18:45:10 +0200

    insert basepath and assetprefix manually

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-const isProduction = process.env.NODE_ENV === 'production';
 const nextConfig = {
-    basePath: isProduction ? '/agencycentral-landing-page' : '',
-    assetPrefix: isProduction ? '/agencycentral-landing-page/' : '',
+    basePath: '/agencycentral-landing-page',
+    assetPrefix: '/agencycentral-landing-page/',
     output: 'export',
     distDir: 'deployment',
     // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
