71b92484c756aaf27dd9bca16fc9e6f37c6897ee
tristangms <nvisionfactory@gmail.com>
2024-09-19 06:44:22 +0200

    Fix favicon links by updating basepath and assetprefix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,10 +16,10 @@ export default function RootLayout({
   return (
     <html lang="en">
         <head>
-        <link rel="icon" href="/favicon/favicon.ico" sizes="any" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
+        <link rel="icon" href="/agencycentral-landing-page/favicon/favicon.ico" sizes="any" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/agencycentral-landing-page/favicon/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/agencycentral-landing-page/favicon/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/agencycentral-landing-page/favicon/favicon-16x16.png"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
         </head>
         <body className={dmSans.className}>{children}</body>
